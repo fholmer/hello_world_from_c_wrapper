9ef96d1017b7bd1c97b2c74d324dea377e50e9a9
fh <none@none>
2019-04-08 17:24:42 +0200

    we need a .lib-file

--- other_src/build.cmd
+++ other_src/build.cmd
@@ -1,2 +1,7 @@
+del world.dll
+del world.exp
+del world.lib
+del world.obj
+
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 cl /LD world.c
--- other_src/world.h
+++ other_src/world.h
@@ -1,3 +1,10 @@
-void hello_world();
-void hello(const char* name);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+__declspec(dllexport) void hello_world();
+__declspec(dllexport) void hello(const char* name);
+
+#ifdef __cplusplus
+}
+#endif
