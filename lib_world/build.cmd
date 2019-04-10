6aa922bbd046b6c15f6ef0cc3fb07ea349193fcf
fh <none@none>
2019-04-10 11:59:18 +0200

    windows builds into bin/win

--- lib_world/build.cmd
+++ lib_world/build.cmd
@@ -1,11 +1,7 @@
 :: ps husk å lage mappen bin/win hvis den ikke finnes
-
-del bin/win/world.dll
-del world.exp
-del bin/win/world.lib
-del world.obj
+if not exist bin mkdir bin
+if not exist bin\win mkdir bin\win
+::del bin\win\*
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-cl /LD world.c
-move world.dll bin/win/
-move world.lib bin/win/
+cl /LD -Fe:bin/win/world -Fo:bin/win/world src/world.c
